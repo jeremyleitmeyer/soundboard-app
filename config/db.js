bd48e80f51a23622af6c6f9e9590e7dcb194706e
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2018-03-06 17:46:50 -0500

    more setup for routes, models, etc.

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
-
-
-'mongodb://jeremy:7A1s2d98@ds257808.mlab.com:57808/soundboard-dev'
+module.exports = {
+    'url' : 'mongodb://jeremy:7A1s2d98@ds257808.mlab.com:57808/soundboard-dev'
+};
--- routes.js
+++ routes.js
@@ -1,12 +1,63 @@
-// use res.render to load up an ejs view file
-app.get('/', function(req, res) {
+module.exports = function (app, passport) {
+
+  // use res.render to load up an ejs view file
+  app.get('/', function (req, res) {
     var data = 'sounds I hope'
     res.render('pages/index', {
-        data: data
+      data: data
     });
-});
+  });
+
+  // show the login form
+  app.get('/login', function (req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('login.ejs', {
+      message: req.flash('loginMessage')
+    });
+  });
+
+  // process the login form
+  // app.post('/login', do all our passport stuff here);
+
+  // show the signup form
+  app.get('/signup', function (req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', {
+      message: req.flash('signupMessage')
+    });
+  });
+
+  // process the signup form
+  // app.post('/signup', do all our passport stuff here);
 
 
-// app.get('/user=id/settings', function(req, res){
-//     res.render('pages/user_settings')
-// })
+  // we will want this protected so you have to be logged in to visit
+  // we will use route middleware to verify this (the isLoggedIn function)
+  app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('profile.ejs', {
+      user: req.user // get the user out of session and pass to template
+    });
+  });
+
+  // app.get('/user=id/settings', function(req, res){
+  //     res.render('pages/user_settings')
+  // })
+
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
